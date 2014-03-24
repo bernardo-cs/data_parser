ad2e739350af20670d358cd16cad74dedb4d2e94
tufa <bersimoes@gmail.com>
2014-03-24 16:21:49 +0000

    refactored data parser into bin_matrix and jsondata

--- lib/data_parser.rb
+++ lib/data_parser.rb
@@ -4,6 +4,13 @@ module DataParser
   def self.parse
     "Hello there"
   end
+  def self.root
+    File.expand_path('../../', __FILE__)
+  end
+  def self.spec
+    File.join root, 'spec'
+  end
   require 'data_parser/tweet_eater'
+  require 'data_parser/json_data'
 end
 
