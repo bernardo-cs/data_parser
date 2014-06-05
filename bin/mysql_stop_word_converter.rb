8e3507412b562f5f40044aba43b1a21f1bd9ed6b
Bernardo <bersimoes@gmail.com>
2014-06-05 15:37:12 +0100

    added new file of trimed stopwords, more fit for stop word removal on trimmed tweets

--- bin/mysql_stop_word_converter.rb
+++ bin/mysql_stop_word_converter.rb
@@ -1,3 +1,6 @@
 require_relative '../lib/data_parser'
 
-puts IO.read('storage/mysql_stop_words.txt').split("\n").map{ |line| line.squeeze }
+require 'set'
+File.open('storage/mysql_stop_words_trimed.txt', 'w') do |file|
+  file.puts Set.new(IO.read('storage/mysql_stop_words.txt').split("\n").map{ |line| line.remove_url.remove_non_letters.downcase.squeeze.remove_small_words }.select{ |word| word.size > 1 }).to_a
+end
--- lib/data_parser/string.rb
+++ lib/data_parser/string.rb
@@ -1,10 +1,16 @@
 require 'uri'
 
 class String
-  STOP_WORDS_CACHE = IO.read(File.join(DataParser.storage,'mysql_stop_words.txt')).split   
+  @stop_words_cache = nil
+  @stop_words_trimed_cache = nil
 
   def trim
-     remove_url.remove_stop_words.remove_non_letters.downcase.squeeze.remove_small_words
+     remove_url
+     .remove_stop_words
+     .remove_non_letters
+     .downcase
+     .squeeze
+     .remove_small_words
   end
 
   def remove_small_words
@@ -27,7 +33,11 @@ class String
     split.select{|word| stop_words.include?(word)}
   end
 
+  def stop_words_trimed
+    @stop_words_trimed_cache ||= IO.read(File.join(DataParser.storage,'mysql_stop_words_trimed.txt')).split   
+  end
+
   def stop_words
-    STOP_WORDS_CACHE
+    @stop_words_cache ||= IO.read(File.join(DataParser.storage,'mysql_stop_words.txt')).split   
   end
 end
