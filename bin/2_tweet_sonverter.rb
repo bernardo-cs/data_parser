e66f8201bb66983d53e3f6e4b2bcbcd4b32ca90c
Bernardo <bersimoes@gmail.com>
2014-06-09 18:06:18 +0100

    added filter for csv files

--- bin/2_tweet_sonverter.rb
+++ bin/2_tweet_sonverter.rb
@@ -12,7 +12,7 @@ folder_server = '~/coding/twitter_data_francisco/decompressed'
 # Get all files in this folder that start with tweets
 puts folder_local
 Dir[File.join( folder_local , "tweets*" )].each do |file_path|
-  unless file_path.match(/\.wcount/) || file_path.match(/english/) 
+  unless file_path.match(/\.wcount/) || file_path.match(/\.csv/) 
     puts "parsing file #{File.basename(file_path)}..."
     JSONData.new(File.dirname(file_path), File.basename(file_path), File.basename(file_path) + "_english_trimed.csv").to_csv
   end
