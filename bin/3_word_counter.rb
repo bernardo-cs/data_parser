1a5d267783c65831ab29084de6b7b8104e62f48b
Bernardo Simoes <bersimoes@gmail.com>
2014-06-17 17:47:57 +0100

    fixed files for production server

--- bin/3_word_counter.rb
+++ bin/3_word_counter.rb
@@ -1,5 +1,6 @@
 ## Counts the number of word ocurrences in the csv file
 #  stores it in a file with the same name, with extension .wcount
+#  counts only the third [2] columns of the csv file
 
 def file_reader file_name
   @words_count = {}
@@ -8,7 +9,7 @@ def file_reader file_name
 end
 
 def count_words line
-  line.split(',')[1].split(' ').each{ |word| count( word ) }
+  line.split(' ').each{ |word| count( word ) }
 end
 
 def count word
@@ -21,10 +22,15 @@ def hash_to_file hash, file_name
   end
 end
 
-data_set_path = '/src/thesis/inesc_data_set_sample/decompressed/*_english_trimed.csv'
-data_set_server_path = '/home/bersimoes/coding/twitter_data_francisco/decompressed'
-Dir.new(data_set_path).each do |file_name|
-  if file_name.match /csv/
-    file_reader("../decompressed/" + file_name)
+folder_local = '/src/thesis/inesc_data_set_sample/decompressed'
+folder_server = '/home/bersimoes/coding/twitter_data_francisco/decompressed/'
+# Get all files in this folder that start with tweets
+Dir.foreach( folder_server ) do |file_path|
+  next if file_path =='.' || file_path == '..'
+  unless file_path.match(/\.wcount/) 
+    if file_path.match /_trimed.csv/
+      puts "counting words file #{File.basename(file_path)}..."
+      file_reader( folder_server + file_path)
+    end
   end
 end
--- bin/4_global_word_counter.rb
+++ bin/4_global_word_counter.rb
@@ -2,19 +2,23 @@
 ## and returns a file with all the words present ind the dataset
 ## counted
 
-data_set_path = '/src/thesis/inesc_data_set_sample/decompressed'
-file_extensions = '*.wcount'
+data_set_path ='/home/bersimoes/coding/twitter_data_francisco/decompressed/'
 words_count = {}
+
 # Iterates through all the files and adds the ocurrence of words to a new file
-Dir.new(File.join(data_set_path, file_extensions)).each do |file_name|
-  File.read(file_name).each_line do |line|
-    if words_count.has_key?( line.split(',')[0] )
-      words_count[line.split(',').[0]] = words_count[line.split(',').[0]] + line.split(',')[1].to_i
-    else
-      words_count[line.split(',').[0]] = line.split(',')[1].to_i
+Dir.foreach( data_set_path ) do |file_path|
+  next if file_path =='.' || file_path == '..' || file_path.match(/\.swp/)
+  if file_path.match /\.wcount/ 
+    puts "counting words file #{File.basename(file_path)}..."
+    File.read(data_set_path + file_path).each_line do |line|
+      if words_count.has_key?( line.split(',')[0] )
+        words_count[line.split(',')[0]] = words_count[line.split(',')[0]] + line.split(',')[1].to_i
+      else
+        words_count[line.split(',')[0]] = line.split(',')[1].to_i
+      end
     end
   end
-end      
+end
 
 def hash_to_file hash, file_name, data_set_path
   File.open(File.join(data_set_path, file_name), 'w') do |file|
@@ -22,4 +26,4 @@ def hash_to_file hash, file_name, data_set_path
   end
 end
 
-hash_to_file(words_count, 'data_set_words')
+hash_to_file(words_count, 'data_set_words', data_set_path)
